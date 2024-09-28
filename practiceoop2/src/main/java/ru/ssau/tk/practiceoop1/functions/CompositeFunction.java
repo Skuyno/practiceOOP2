4b3d32c9669c97e3d12a4befe21c7b80a6a9fe60
Skuyno <143557346+Skuyno@users.noreply.github.com>
2024-09-29 00:35:43 +0400

    Добавлен класс CompositeFunction, а также подготовлена заготовка под юнит тесты для проверки его работы.

---
+++ practiceoop2/src/main/java/ru/ssau/tk/practiceoop1/functions/CompositeFunction.java
@@ -0,0 +1,17 @@
+package ru.ssau.tk.practiceoop1.functions;
+
+public class CompositeFunction implements MathFunction {
+
+    private final MathFunction firstFunction;
+    private final MathFunction secondFunction;
+
+    public CompositeFunction(MathFunction firstFunction, MathFunction secondFunction){
+        this.firstFunction = firstFunction;
+        this.secondFunction = secondFunction;
+    }
+
+    @Override
+    public double apply(double x) {
+        return secondFunction.apply(firstFunction.apply(x));
+    }
+}
---
+++ practiceoop2/src/test/java/ru/ssau/tk/practiceoop1/functions/CompositeFunctionTest.java
@@ -0,0 +1,8 @@
+package ru.ssau.tk.practiceoop1.functions;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CompositeFunctionTest {
+
+}
