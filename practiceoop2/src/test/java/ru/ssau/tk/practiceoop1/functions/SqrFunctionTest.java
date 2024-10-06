1f61a8f2e70edded48c57df9672ca7529504f364
Stepanova <zakazakaxy@gmail.com>
2024-10-06 22:50:14 +0400

    тест SqrFunction

--- practiceoop2/src/test/java/ru/ssau/tk/practiceoop1/functions/SqrFunctionTest.java
+++ practiceoop2/src/test/java/ru/ssau/tk/practiceoop1/functions/SqrFunctionTest.java
@@ -8,11 +8,11 @@ class SqrFunctionTest {
     @Test
     public void testApply() {
         MathFunction sqr = new SqrFunction();
-        assertEquals(-1, sqr.apply(-1), 0.000001);
-        assertEquals(0.00000214, sqr.apply(0.00000214), 0.000001);
+        assertEquals(1, sqr.apply(-1), 0.000001);
+        assertEquals(0.04, sqr.apply(0.2), 0.000001);
         assertEquals(0, sqr.apply(0), 0.000001);
-        assertEquals(-0.00000395, sqr.apply(-0.00000395), 0.000001);
-        assertEquals(-32134267.0, sqr.apply(-32134267.0), 0.000001);
+        assertEquals(0,000009, sqr.apply(-0.0003), 0.000001);
+        assertEquals(4, sqr.apply(-2), 0.000001);
 
 
     }
