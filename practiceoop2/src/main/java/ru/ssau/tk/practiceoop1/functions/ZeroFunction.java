ed9f1f3797916e58dc77930918c3fd3a7a07fc3e
Stepanova <zakazakaxy@gmail.com>
2024-10-06 22:40:45 +0400

    добавлен класс ZeroFunction

---
+++ practiceoop2/src/main/java/ru/ssau/tk/practiceoop1/functions/ZeroFunction.java
@@ -0,0 +1,11 @@
+package ru.ssau.tk.practiceoop1.functions;
+
+public class ZeroFunction implements MathFunction {
+    @Override
+
+    public ZeroFunction(){
+    }
+    public double apply(double x) {
+        return 0;
+    }
+}
