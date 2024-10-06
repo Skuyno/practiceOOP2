ee45c7a0a06b05e82c5b3be23fec7ee972ed7c06
Stepanova <zakazakaxy@gmail.com>
2024-10-06 22:40:09 +0400

    добавлен класс UnitFunction

---
+++ practiceoop2/src/main/java/ru/ssau/tk/practiceoop1/functions/UnitFunction.java
@@ -0,0 +1,11 @@
+package ru.ssau.tk.practiceoop1.functions;
+
+public class UnitFunction implements MathFunction {
+    @Override
+
+    public UnitFunction(){
+    }
+    public double apply(double x) {
+        return 1;
+    }
+}
