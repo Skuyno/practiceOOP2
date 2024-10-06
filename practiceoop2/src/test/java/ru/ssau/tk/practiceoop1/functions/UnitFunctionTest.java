f0caafa7253d9e4c4b1166fa2e54bd67d0830b7b
Stepanova <zakazakaxy@gmail.com>
2024-10-06 22:51:02 +0400

    тест UnitFunction

---
+++ practiceoop2/src/test/java/ru/ssau/tk/practiceoop1/functions/UnitFunctionTest.java
@@ -0,0 +1,19 @@
+package ru.ssau.tk.practiceoop1.functions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UnitFunctionTest {
+    @Test
+    public void testApply() {
+        MathFunction unit = new UnitFunction();
+
+        assertEquals(1, identity.apply(23412867.0), 0.000001);
+        assertEquals(1, identity.apply(0.00000214), 0.000001);
+        assertEquals(1, identity.apply(0), 0.000001);
+        assertEquals(1, identity.apply(-0.00000395), 0.000001);
+        assertEquals(1, identity.apply(-32134267.0), 0.000001);
+    }
+
+}
