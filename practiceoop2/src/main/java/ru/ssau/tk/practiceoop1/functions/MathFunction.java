6d2758c91ef19dcd573fbba2302bd70f2220ccee
Stepanova <zakazakaxy@gmail.com>
2024-10-07 18:24:21 +0400

    добавлен метод andThen

--- practiceoop2/src/main/java/ru/ssau/tk/practiceoop1/functions/MathFunction.java
+++ practiceoop2/src/main/java/ru/ssau/tk/practiceoop1/functions/MathFunction.java
@@ -2,4 +2,8 @@ package ru.ssau.tk.practiceoop1.functions;
 
 public interface MathFunction {
     double apply(double x);
+
+    default CompositeFunction andThen(MathFunction afterFunction) {
+        return (double x) -> afterFunction.apply(this.apply(x));
+    }
 }
