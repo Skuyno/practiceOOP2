e6e0abe7b22a41fe31a4437e6477f835e538999a
Stepanova <zakazakaxy@gmail.com>
2024-10-10 22:05:30 +0400

    добавлен тест remove в ArrayTabulatedFunctionTest

--- practiceoop2/src/test/java/ru/ssau/tk/practiceoop1/functions/ArrayTabulatedFunctionTest.java
+++ practiceoop2/src/test/java/ru/ssau/tk/practiceoop1/functions/ArrayTabulatedFunctionTest.java
@@ -1,5 +1,6 @@
 package ru.ssau.tk.practiceoop1.functions;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -34,5 +35,43 @@ class ArrayTabulatedFunctionTest {
         assertEquals(6, function.getCount());
         assertEquals(6.0, function.getX(5));
         assertEquals(7.0, function.getY(5));
+    }
+
+    private ArrayTabulatedFunction function;
+
+    @BeforeEach
+    public void setUp() {
+        double[] xValues = {1.0, 2.0, 3.0};
+        double[] yValues = {1.0, 4.0, 9.0};
+        function = new ArrayTabulatedFunction(xValues, yValues);
+    }
+
+    @Test
+    public void testRemoveMiddleElement() {
+        function.remove(1); // Удаляем элемент с индексом 1 (x = 2.0)
+
+        assertEquals(2, function.getCount());
+        assertEquals(1.0, function.getX(0));
+        assertEquals(3.0, function.getX(1)); // Теперь x = 3.0 на месте x = 1
+        assertEquals(1.0, function.getY(0));
+        assertEquals(9.0, function.getY(1)); // y = 9.0 на месте y = 1
+    }
+
+    @Test
+    public void testRemoveFirstElement() {
+        function.remove(0); // Удаляем первый элемент (x = 1.0)
+
+        assertEquals(2, function.getCount());
+        assertEquals(2.0, function.getX(0)); // Теперь x = 2.0 должен быть на месте 0
+        assertEquals(4.0, function.getY(0)); // y = 4.0 теперь на месте 0
+    }
+
+    @Test
+    public void testRemoveLastElement() {
+        function.remove(2); // Удаляем последний элемент (x = 3.0)
+
+        assertEquals(2, function.getCount());
+        assertEquals(1.0, function.getX(0)); // x = 1.0 на месте 0
+        assertEquals(4.0, function.getY(1)); // y = 4.0 должен остаться на месте
     }
 }
