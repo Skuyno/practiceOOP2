c45d8b9c56b87e2fd9078225a979f83a9cc72485
Stepanova <zakazakaxy@gmail.com>
2024-10-18 21:29:29 +0400

    добавлен Point

---
+++ practiceoop2/src/main/java/ru/ssau/tk/practiceoop1/functions/Point.java
@@ -0,0 +1,10 @@
+package ru.ssau.tk.practiceoop1.functions;
+
+public class Point {
+    public final double x;
+    public final double y;
+    public Point(double x, double y){
+        this.x=x;
+        this.y=y;
+    }
+}
