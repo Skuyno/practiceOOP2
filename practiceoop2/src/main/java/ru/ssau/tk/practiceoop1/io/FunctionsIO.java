093277a948a5d8d32a59e439d18d1a7f2b451b21
Stepanova <zakazakaxy@gmail.com>
2024-10-22 21:47:12 +0400

    Создание пакете io и класса FunctionsIO

---
+++ practiceoop2/src/main/java/ru/ssau/tk/practiceoop1/io/FunctionsIO.java
@@ -0,0 +1,8 @@
+package ru.ssau.tk.practiceoop1.io;
+
+public final class FunctionsIO {
+    // Приватный конструктор, чтобы предотвратить создание экземпляров
+    private FunctionsIO() {
+        throw new UnsupportedOperationException("Cannot instantiate utility class");
+    }
+}
