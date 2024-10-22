d7e1dd438927f9bfee71cf6d4ae84cf2777a9e57
Skuyno <143557346+Skuyno@users.noreply.github.com>
2024-10-23 03:09:18 +0400

    В класс FunctionsIO добавлен метод writeTabulatedFunction. Добавлен класс TabulatedFunctionFileOutputStream

--- practiceoop2/src/main/java/ru/ssau/tk/practiceoop1/io/FunctionsIO.java
+++ practiceoop2/src/main/java/ru/ssau/tk/practiceoop1/io/FunctionsIO.java
@@ -1,8 +1,28 @@
 package ru.ssau.tk.practiceoop1.io;
 
+import ru.ssau.tk.practiceoop1.functions.Point;
+import ru.ssau.tk.practiceoop1.functions.TabulatedFunction;
+
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
 public final class FunctionsIO {
     // Приватный конструктор, чтобы предотвратить создание экземпляров
     private FunctionsIO() {
         throw new UnsupportedOperationException("Cannot instantiate utility class");
     }
+
+    public static void writeTabulatedFunction(BufferedOutputStream outputStream, TabulatedFunction function) throws IOException {
+        DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+
+        dataOutputStream.writeInt(function.getCount());
+        for (Point point : function) {
+            dataOutputStream.writeDouble(point.x);
+            dataOutputStream.writeDouble(point.y);
+        }
+
+        dataOutputStream.flush();
+    }
+
 }
