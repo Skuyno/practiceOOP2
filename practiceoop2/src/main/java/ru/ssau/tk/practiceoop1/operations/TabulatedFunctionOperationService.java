8309984b68cefcc705e745b6c7b9b91b8902a587
Stepanova <zakazakaxy@gmail.com>
2024-10-22 21:39:04 +0400

    Создание InconsistentFunctionsException, обновление TabulatedFunctionOperationService и тестирование

--- practiceoop2/src/main/java/ru/ssau/tk/practiceoop1/operations/TabulatedFunctionOperationService.java
+++ practiceoop2/src/main/java/ru/ssau/tk/practiceoop1/operations/TabulatedFunctionOperationService.java
@@ -1,10 +1,32 @@
 package ru.ssau.tk.practiceoop1.operations;
 
+import ru.ssau.tk.practiceoop1.exceptions.InconsistentFunctionsException;
 import ru.ssau.tk.practiceoop1.functions.TabulatedFunction;
 import ru.ssau.tk.practiceoop1.functions.Point;
+import ru.ssau.tk.practiceoop1.functions.factory.ArrayTabulatedFunctionFactory;
+import ru.ssau.tk.practiceoop1.functions.factory.TabulatedFunctionFactory;
 
 public class TabulatedFunctionOperationService {
-    public static Point[] asPoints(TabulatedFunction tabulatedFunction){
+    private TabulatedFunctionFactory factory;
+
+    public TabulatedFunctionOperationService() {
+        this.factory = new ArrayTabulatedFunctionFactory(); // или другой тип фабрики
+    }
+
+    public TabulatedFunctionOperationService(TabulatedFunctionFactory factory) {
+        this.factory = factory;
+    }
+
+    public TabulatedFunctionFactory getFactory() {
+        return factory;
+    }
+
+    public void setFactory(TabulatedFunctionFactory factory) {
+        this.factory = factory;
+    }
+
+
+    public static Point[] asPoints(TabulatedFunction tabulatedFunction) {
         int count = tabulatedFunction.getCount(); // Предполагаем, что есть метод getCount()
         Point[] points = new Point[count];
 
@@ -15,5 +37,41 @@ public class TabulatedFunctionOperationService {
         }
 
         return points;
-    };
+    }
+
+    ;
+
+    private TabulatedFunction doOperation(TabulatedFunction a, TabulatedFunction b, BiOperation operation) throws InconsistentFunctionsException {
+        if (a.getCount() != b.getCount()) {
+            throw new InconsistentFunctionsException("Functions have different number of points.");
+        }
+
+        Point[] pointsA = asPoints(a);
+        Point[] pointsB = asPoints(b);
+
+        double[] xValues = new double[pointsA.length];
+        double[] yValues = new double[pointsA.length];
+
+        for (int i = 0; i < pointsA.length; i++) {
+            if (pointsA[i].x != pointsB[i].x) {
+                throw new InconsistentFunctionsException("Functions have different x values at index " + i);
+            }
+            xValues[i] = pointsA[i].x;
+            yValues[i] = operation.apply(pointsA[i].y, pointsB[i].y);
+        }
+
+        return factory.create(xValues, yValues);
+    }
+
+    public TabulatedFunction add(TabulatedFunction a, TabulatedFunction b) throws InconsistentFunctionsException {
+        return doOperation(a, b, (u, v) -> u + v);
+    }
+
+    public TabulatedFunction subtract(TabulatedFunction a, TabulatedFunction b) throws InconsistentFunctionsException {
+        return doOperation(a, b, (u, v) -> u - v);
+    }
+
+    private interface BiOperation {
+        double apply(double u, double v);
+    }
 }
--- practiceoop2/src/test/java/ru/ssau/tk/practiceoop1/functions/TabulatedFunctionOperationServiceTest.java
+++ practiceoop2/src/test/java/ru/ssau/tk/practiceoop1/functions/TabulatedFunctionOperationServiceTest.java
@@ -1,6 +1,7 @@
 package ru.ssau.tk.practiceoop1.functions;
 
 import org.junit.jupiter.api.Test;
+import ru.ssau.tk.practiceoop1.exceptions.InconsistentFunctionsException;
 import ru.ssau.tk.practiceoop1.operations.TabulatedFunctionOperationService;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,5 +21,47 @@ public class TabulatedFunctionOperationServiceTest {
         assertEquals(20.0, points[1].y);
         assertEquals(3.0, points[2].x);
         assertEquals(30.0, points[2].y);
+    }
+
+    @Test
+    public void testAddition() throws InconsistentFunctionsException {
+        TabulatedFunction function1 = new ArrayTabulatedFunction(new double[]{1.0, 2.0, 3.0}, new double[]{10.0, 20.0, 30.0});
+        TabulatedFunction function2 = new ArrayTabulatedFunction(new double[]{1.0, 2.0, 3.0}, new double[]{5.0, 15.0, 25.0});
+
+        TabulatedFunction result = new TabulatedFunctionOperationService().add(function1, function2);
+
+        assertEquals(3, result.getCount());
+        assertEquals(1.0, result.getX(0));
+        assertEquals(15.0, result.getY(0)); // 10 + 5
+        assertEquals(2.0, result.getX(1));
+        assertEquals(35.0, result.getY(1)); // 20 + 15
+        assertEquals(3.0, result.getX(2));
+        assertEquals(55.0, result.getY(2)); // 30 + 25
+    }
+
+    @Test
+    public void testSubtraction() throws InconsistentFunctionsException {
+        TabulatedFunction function1 = new ArrayTabulatedFunction(new double[]{1.0, 2.0, 3.0}, new double[]{10.0, 20.0, 30.0});
+        TabulatedFunction function2 = new ArrayTabulatedFunction(new double[]{1.0, 2.0, 3.0}, new double[]{5.0, 15.0, 25.0});
+
+        TabulatedFunction result = new TabulatedFunctionOperationService().subtract(function1, function2);
+
+        assertEquals(3, result.getCount());
+        assertEquals(1.0, result.getX(0));
+        assertEquals(5.0, result.getY(0)); // 10 - 5
+        assertEquals(2.0, result.getX(1));
+        assertEquals(5.0, result.getY(1)); // 20 - 15
+        assertEquals(3.0, result.getX(2));
+        assertEquals(5.0, result.getY(2)); // 30 - 25
+    }
+
+    @Test
+    public void testInconsistentFunctions() {
+        TabulatedFunction function1 = new ArrayTabulatedFunction(new double[]{1.0, 2.0}, new double[]{10.0, 20.0});
+        TabulatedFunction function2 = new ArrayTabulatedFunction(new double[]{1.0}, new double[]{5.0});
+
+        assertThrows(InconsistentFunctionsException.class, () -> {
+            new TabulatedFunctionOperationService().add(function1, function2);
+        });
     }
 }
