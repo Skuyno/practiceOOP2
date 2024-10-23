a4dd43150995ed40cbcdfaf1dc8987b580c7dc6d
Skuyno <143557346+Skuyno@users.noreply.github.com>
2024-10-23 23:27:32 +0400

    Небольшие фиксы

--- practiceoop2/src/main/java/ru/ssau/tk/practiceoop1/io/TabulatedFunctionFileWriter.java
+++ practiceoop2/src/main/java/ru/ssau/tk/practiceoop1/io/TabulatedFunctionFileWriter.java
@@ -15,8 +15,8 @@ public class TabulatedFunctionFileWriter {
         TabulatedFunction linkedListFunction = new LinkedListTabulatedFunction(new double[]{1.0, 2.0, 3.0}, new double[]{1.0, 4.0, 9.0});
 
         // Записываем функции в файлы
-        try (BufferedWriter arrayWriter = new BufferedWriter(new FileWriter("C:/Users/Admin/IdeaProjects/practiceOOP2new/practiceoop2/output/array_function.txt"));
-             BufferedWriter linkedListWriter = new BufferedWriter(new FileWriter("C:/Users/Admin/IdeaProjects/practiceOOP2new/practiceoop2/output/linked_list_function.txt"))) {
+        try (BufferedWriter arrayWriter = new BufferedWriter(new FileWriter("output/array_function.txt"));
+             BufferedWriter linkedListWriter = new BufferedWriter(new FileWriter("output/linked_list_function.txt"))) {
 
             FunctionsIO.writeTabulatedFunction(arrayWriter, arrayFunction);
             FunctionsIO.writeTabulatedFunction(linkedListWriter, linkedListFunction);
