d5cc46b40e23c70e15cc8f8f122ebeda631e79d5
Stepanova <zakazakaxy@gmail.com>
2024-10-23 14:49:21 +0400

    Создание классов SteppingDifferentialOperator, LeftSteppingDifferentialOperator,RightSteppingDifferentialOperator, MiddleSteppingDifferentialOperator и тест

---
+++ practiceoop2/src/main/java/ru/ssau/tk/practiceoop1/operations/MiddleSteppingDifferentialOperator.java
@@ -0,0 +1,22 @@
+package ru.ssau.tk.practiceoop1.operations;
+
+import ru.ssau.tk.practiceoop1.functions.MathFunction;
+
+public class MiddleSteppingDifferentialOperator extends SteppingDifferentialOperator {
+
+    public MiddleSteppingDifferentialOperator(double step) {
+        super(step);
+    }
+
+    @Override
+    public MathFunction derive(MathFunction function) {
+        return new MathFunction() {
+            @Override
+            public double apply(double x) {
+                return (function.apply(x + step / 2) - function.apply(x - step / 2)) / step;
+            }
+        };
+    }
+
+
+}
---
+++ practiceoop2/src/main/java/ru/ssau/tk/practiceoop1/operations/SteppingDifferentialOperator.java
@@ -0,0 +1,32 @@
+package ru.ssau.tk.practiceoop1.operations;
+
+import ru.ssau.tk.practiceoop1.functions.MathFunction;
+
+public abstract class SteppingDifferentialOperator implements DifferentialOperator<MathFunction> {
+    protected double step;
+
+    public SteppingDifferentialOperator(double step) {
+        if (step <= 0 || Double.isInfinite(step) || Double.isNaN(step)) {
+            throw new IllegalArgumentException("Step must be positive and finite.");
+        }
+        this.step = step;
+    }
+
+    public double getStep() {
+        return step;
+    }
+
+    public void setStep(double step) {
+        if (step <= 0 || Double.isInfinite(step) || Double.isNaN(step)) {
+            throw new IllegalArgumentException("Step must be positive and finite.");
+        }
+        this.step = step;
+    }
+
+    public MathFunction derive() {
+        return derive(null);
+    }
+
+    @Override
+    public abstract MathFunction derive(MathFunction function);
+}
